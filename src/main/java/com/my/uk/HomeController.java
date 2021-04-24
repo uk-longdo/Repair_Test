1f2a2080ddaf1d2591fd1e9fd0f297fefe283103
uk-longdo <alsdnr729@gmail.com>
2021-04-24 19:28:03 +0900

    설정

--- src/main/java/com/my/uk/HomeController.java
+++ src/main/java/com/my/uk/HomeController.java
@@ -1,5 +1,7 @@
 package com.my.uk;
 
+
+
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -11,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 /**
  * Handles requests for the application home page.
  */
@@ -19,10 +22,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -33,7 +33,8 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "repair_main";
 	}
+
 	
 }
---
+++ src/main/webapp/WEB-INF/views/repair_main.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+   <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+<div>
+	<span>회원정보</span>
+	<span>회원정보 등록</span>
+</div>
+
+
+</body>
+</html>
